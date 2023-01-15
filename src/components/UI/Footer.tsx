23ff140765f83d554ebb7dcaed3b251d7789427f
olawanle temitayo <olawanletemitayo@gmail.com>
2023-01-15 23:56:29 +0100

    made the footer section

---
+++ src/components/UI/Footer.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <SocialContainer>
+        <SocialIcon src={fb} alt="Facebook" />
+        <SocialIcon src={twitter} alt="Twitter" />
+        <SocialIcon src={instagram} alt="Instagram" />
+        <SocialIcon src={youtube} alt="Youtube" />
+      </SocialContainer>
+      <FooterText>
+        <p>© 2021 Movix</p>
+      </FooterText>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
+
+const FooterContainer = styled.footer`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  padding: 20px 0;
+  font-family: 'DM Sans', sans-serif;
+  font-size: 0.8rem;
+  color: #000000;
+  gap: 20px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const SocialIcon = styled.img`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+`;
+
+const FooterText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
